2377fbc97e09d5e4502b76029d0608e258362ea7
Lahiru <lahiru7675@gmail.com>
2023-09-03 14:05:12 +1000

    project files

---
+++ include/GamePlay.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+#include <array>
+
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+#include "State.hpp"
+#include "Game.hpp"
+#include "Snake.hpp"
+
+class GamePlay : public Engine::State
+{
+
+private:
+    std::shared_ptr<Context>m_context;
+    sf::Sprite m_grass;
+    sf::Sprite m_food;
+    std::array<sf::Sprite,4> m_walls;
+
+    Snake m_snake;
+
+    sf::Vector2f m_snakeDirection;
+    sf::Time m_elapsedTime;
+
+public:
+   GamePlay(std::shared_ptr<Context> &context);
+   ~GamePlay();
+
+         void Init() override;
+         void ProcessInput() override;
+         void Update(const sf::Time deltaTime) override;
+         void Draw() override;
+         void Pause()override;
+         void Start()override;
+
+
+
+};
